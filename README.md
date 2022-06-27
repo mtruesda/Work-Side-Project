4c872e9cddf50d59d57c3d2a9764c7b0c8cfac29
mtruesda <mtruesda@gmail.com>
2022-06-27 18:47:07 -0400

    Roughly usable infinitime helper

--- README.md
+++ README.md
@@ -8,4 +8,9 @@ The goal is to also have a page where you can write and store the number of hour
 Project can create a tkinter window.
 
 @Version 06/27/22
-Project creates the window with a label and a horizontally scrolling text box that can be exited. The project also spawns the textbox with the contents of a savefile called saveFile.txt
+Project creates the window with a label and a horizontally scrolling text box that can be exited. The project also spawns the textbox with the contents of a savefile called savefile.txt. The number ofhours information is stored in a file called hours.txt. When you click the swap pages button you can now swap between the hours page and the scheduling page. The schedule template is set up for one   week and the hours page has a few weeks written onto it. The total counter is not yet setup to automatically place but I may just throw in a calculator. Tkinter isn't incredibly supportive of         scrolling frames and so that makes scrolling through labels difficult (also would've been tedious).
+
+Additional Thoughts: Would be cool to implement a few of these features
+- freezing panes for the names on the hours page (would require doing away with the text box or making a second one that isn't a part of the scrolling one)
+- A total column that totals automatically like in excel or google sheets
+- Templates that fit a bit more securely and can't be edited out of the "software"
--- tracker.py
+++ tracker.py
@@ -1,6 +1,6 @@
 # 
 # Author: Myron Truesdale
-# @@Version 05/21/22
+# @@Version 06/27/22
 # 
 
 # imports
@@ -15,9 +15,40 @@ class gui ():
         self.root.geometry = ("1400x1200")
         self.root.title("Scheduling Assistant")
 
+        self.setupSchedulePage()
+
+    def updateScheduleFile(self):
+        # opens the file and then writes it to the doc from the text box
+        self.saveFile = open("savefile.txt","w")
+        contents = self.text.get("1.0","end")
+        self.saveFile.write(contents)
+        self.saveFile.close()
+
+    def updateHoursFile(self):
+        self.saveFile = open("hours.txt","w")
+        contents = self.hoursText.get("1.0","end")
+        self.saveFile.write(contents)
+        self.saveFile.close()
+
+    def deleteItems(self):
+        ## DELETION
+
+        # pack forgets 
+        self.labelTwo.pack_forget()
+        self.secondScroller.pack_forget()
+        self.hoursText.pack_forget()
+        self.saveHours.pack_forget()
+        self.swapToSchedule.pack_forget()
+
+        self.setupSchedulePage()
+
+
+    def setupSchedulePage(self):
+        ## SETUP
+
         # top label
-        self.label = ttk.Label(self.root, text="Daily Employees Page", padding=10)
-        self.label.pack()  
+        self.labelOne = ttk.Label(self.root, text="Daily Employees Page", padding=10)
+        self.labelOne.pack()  
 
         # scroller stuff
         self.scroller = Scrollbar(self.root, orient='horizontal') 
@@ -25,7 +56,7 @@ class gui ():
 
         # text 
         self.text = Text(self.root, font=("Calibri, 16"), wrap=NONE, xscrollcommand=self.scroller.set)
-        self.text.pack() 
+        self.text.pack()  
 
         # add from file here 
         self.saveFile = open("savefile.txt", "r")
@@ -35,16 +66,67 @@ class gui ():
 
         self.saveFile.close()
 
-        # attach scroller
+        # attach scrollers
         self.scroller.config(command=self.text.xview)
+        
+        # make a save button 
+        self.saveSchedule = ttk.Button(self.root, text='Save Schedule', command=self.updateScheduleFile)
+        self.saveSchedule.pack()
 
-
+        # swap to hours page button
+        self.swapToHours = ttk.Button(self.root, text='Swap to Hours Page', command=self.setupHoursPage)
+        self.swapToHours.pack()
 
         # mainloop
         self.root.mainloop()
 
-    def update(self):
-        print ('update the saved file here') 
+
+
+    def setupHoursPage(self):
+        ## DELETION
+
+        # pack forgets
+        self.labelOne.pack_forget()
+        self.scroller.pack_forget()
+        self.text.pack_forget()
+        self.saveSchedule.pack_forget()
+        self.swapToHours.pack_forget()
+
+        ## SETUP
+        
+        # second top label 
+        self.labelTwo = ttk.Label(self.root, text='Employee Hours Page', padding=10)
+        self.labelTwo.pack()
+
+        # scroller two stuff 
+        self.secondScroller = Scrollbar(self.root, orient='vertical')
+        self.secondScroller.pack(side=RIGHT,fill='y')
+
+        # hours text 
+        self.hoursText = Text(self.root, font=("Calibri, 16"), wrap=NONE,yscrollcommand=self.secondScroller.set)
+        self.hoursText.pack()
+
+        # write the text in from File 
+        self.saveFile = open("hours.txt","r")
+        
+        for line in self.saveFile.readlines():
+            self.hoursText.insert(END, line)    
+
+        self.saveFile.close()
+
+
+        # attach scroller
+        self.secondScroller.config(command=self.hoursText.yview)
+
+        # save hours button
+        self.saveHours = ttk.Button(self.root, text='Save Hours', command=self.updateHoursFile)
+        self.saveHours.pack()
+
+        self.swapToSchedule = ttk.Button(self.root, text='Swap to Schedule Page', command=self.deleteItems)
+        self.swapToSchedule.pack()
+
+        # mainloop
+        self.root.mainloop()
 
 # project runs here
 gui()
